ccd9c748fc1c891c5b418a293f3879018d07cc57
VoidBytes <alecsandru.s.tiplea@gmail.com>
2025-03-11 17:49:51 +0000

    modified:   defenseBackendPrototype.py

--- defenseBackendPrototype.py
+++ defenseBackendPrototype.py
@@ -2,7 +2,12 @@ import os
 import logging
 import time
 import socket
-import scapy # importing necessary modules
+import scapy.all as scapy # importing necessary modules
+
+bool isUnderAttack = false      #   Bool that will switch based on monitoring
+ipCounts = {}           # ip counter against threshold
+threshold = 0           # Threshold to be decided with team
+suspicious = []         # List of suspicious IPs
 
 # Setting up logger, handler and formatting for the log
 def attackLogger():
@@ -15,3 +20,16 @@ def attackLogger():
     logger.addHandler(handler)
     return
 
+def trafficMonitor(isUnderAttack, ipCounts, suspicious, threshold):
+    while isUnderAttack == false:
+        packets = PcapReader("capture.pcap")       # Captures and reads packets
+        for packet in packets:
+            if packet.hasLayer(DNS) and packet[DNS].qr == 1 and packet[DNS].ancount == 0:   # Checks if the DNS responds using a query (qr)
+                ip = packet[IP].dst
+                ipCounts[ip] = ipCounts.get(ip, 0) + 1
+        for ip, instances in ipCounts.items():              # checks count against threshold and decides whether to add it to the list or pass
+            if instances < threshold:
+                continue
+            else:
+                suspicious.append(ip)
+               
