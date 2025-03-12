781c67c56b3767510597402c04054ccd7bc43530
VoidBytes <alecsandru.s.tiplea@gmail.com>
2025-03-12 16:44:37 +0000

    modified:   defenseBackendPrototype.py || Laid base for attack block

--- defenseBackendPrototype.py
+++ defenseBackendPrototype.py
@@ -2,9 +2,9 @@ import os
 import logging
 import time
 import socket
-import scapy.all as scapy # importing necessary modules
+import scapy # importing necessary modules
 
-bool isUnderAttack = false      #   Bool that will switch based on monitoring
+isUnderAttack = False      #   Bool that will switch based on monitoring
 ipCounts = {}           # ip counter against threshold
 threshold = 0           # Threshold to be decided with team
 suspicious = []         # List of suspicious IPs
@@ -18,18 +18,24 @@ def attackLogger():
     formatting = logging.Formatter('%(levelname)s: %(message)s [%(asctime)s]')
     handler.setFormatter(formatting)
     logger.addHandler(handler)
-    return
 
 def trafficMonitor(isUnderAttack, ipCounts, suspicious, threshold):
-    while isUnderAttack == false:
-        packets = PcapReader("capture.pcap")       # Captures and reads packets
+        packets = scapy.PcapReader("capture.pcap")       # Captures and reads packets
         for packet in packets:
-            if packet.hasLayer(DNS) and packet[DNS].qr == 1 and packet[DNS].ancount == 0:   # Checks if the DNS responds using a query (qr)
-                ip = packet[IP].dst
+            if packet.hasLayer(scapy.DNS) and packet[scapy.DNS].qr == 1 and packet[scapy.DNS].ancount == 0:   # Checks if the DNS responds using a query (qr)
+                ip = packet[scapy.IP].dst
                 ipCounts[ip] = ipCounts.get(ip, 0) + 1
         for ip, instances in ipCounts.items():              # checks count against threshold and decides whether to add it to the list or pass
             if instances < threshold:
                 continue
             else:
                 suspicious.append(ip)
-               
+                for i in suspicious:
+                    if i >= threshold:
+                        isUnderAttack = True           # Sets isUnderAttack to True is number of suspicious IPs reaches or exceeds the threshold
+
+def attackBlocker(isUnderAttack, ipCounts, threshold, suspicious):
+    while isUnderAttack == False:
+        pass
+    while isUnderAttack == True:
+        pass # gonna write this out later
